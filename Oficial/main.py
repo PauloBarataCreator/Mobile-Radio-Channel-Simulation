a186f2b31a5a0bff43fee7cc995de3b872ebc664
Paulo Barata <155594255+PauloBarataCreator@users.noreply.github.com>
2026-06-07 00:05:42 -0300

    Adding main file for simulation

--- Oficial/main.py
+++ Oficial/main.py
@@ -1,40 +1,61 @@
 from binary_number import rand_key
 from noise_awgn import noise
+from numpy import sqrt
+import random
 
-print(noise())
-
-# Cria código binário
+# Cria código binário - Limiar
 N = 3 # number of bits or symbols 10^6
-ip = rand_key(N)
-
+binary_input = rand_key(N)
+binary_input_em_lista = [int(i) for i in list(binary_input)]
 
 # Bpsk modulation
 modulated_sgn = []
-for i in ip:
+for i in binary_input:
     if i == "1":
         modulated_sgn.append(1)
     elif i=="0":
         modulated_sgn.append(-1)
-    # print(i)
-print(ip)
-print(modulated_sgn)
-
 
 # Adicionando Ruído AWGN
-
 sgn_awgn = []
-
-print(noise())
-
-
 
 for i,j in zip(modulated_sgn, noise()):
     sgn_awgn.append(i + j)
 
-# for i in modulated_sgn:
+# Rayleigh fading plus Noise
+sgn_ray_awgn=[]
+for i,j in zip(modulated_sgn, noise()):
+    ch_coeff = sqrt(random.gauss(0,1)**2+random.gauss(0,1)**2)/sqrt(2)
+    sgn_ray_awgn.append(i*ch_coeff +j)
 
-#     i = i + noise()
-#     sgn_awgn.append(i)
+# Correção e estimação
+estimation=[]
+for rx in sgn_ray_awgn:
+    # Correlação
+    c1 = 1*rx
+    c2 = -1*rx
+    # Estimação
+    if c1>c2:
+        estimation.append(1)
+    else:
+        estimation.append(0)
+
+# Decisão
+error=0 # This is no for no/ N
+for i,j in zip(estimation,binary_input_em_lista):
+    if i!=j:
+        error = error + 1
 
 
+# Calcula perro (ber)
+perro = error/len(binary_input_em_lista)
+
+# Eb/No
+
+
+print(binary_input_em_lista)
+print(modulated_sgn)
 print(sgn_awgn)
+print(sgn_ray_awgn)
+print(estimation)
+print(error)
